314cf4e8f025f0516bbf658257278e45a75c79ef
ArtaxJay <okjaceauthor@gmail.com>
2024-12-12 01:19:58 +0100

    Fix: modify redis.js to meet required specifications

--- redis.js
+++ redis.js
@@ -4,46 +4,30 @@ import { promisify } from 'util';
 class RedisClient {
   constructor() {
     this.client = createClient();
-
     this.client.on('error', error => {
-      console.error('Redis Client Error:', error);
+      console.log(`Redis client not connected to server: ${error}`);
     });
   }
 
-  async isAlive() {
-    try {
-      await this.client.connect();
-      return true;
-    } catch (error) {
-      console.error('Redis Connection Error:', error);
-      return false;
-    }
+  isAlive() {
+    return this.client.connected ? true : false;
   }
 
   async get(key) {
-    try {
-      const result = await this.client.get(key);
-      return result;
-    } catch (error) {
-      console.error('Redis Get Error:', error);
-      return null;
-    }
+    const getRedisKey = promisify(this.client.get).bind(this.client);
+    const keyValue = await getRedisKey(key);
+    return keyValue;
   }
 
-  async set(key, value, expiration) {
-    try {
-      await this.client.set(key, value, { EX: expiration });
-    } catch (error) {
-      console.error('Redis Set Error:', error);
-    }
+  async set(key, value, time) {
+    const setRedisKey = promisify(this.client.set).bind(this.client);
+    await setRedisKey(key, value);
+    await this.client.expire(key, time);
   }
 
   async del(key) {
-    try {
-      await this.client.del(key);
-    } catch (error) {
-      console.error('Redis Delete Error:', error);
-    }
+    const removeKeyValue = promisify(this.client.del).bind(this.client);
+    await removeKeyValue(key);
   }
 }
 
